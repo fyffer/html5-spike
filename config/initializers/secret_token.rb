b377ddf77c00f7c3e51aa5245876092bfadfcf75
Tim McGilchrist <timmcgil@gmail.com>
2011-12-13 09:46:00 +1100

    Initial import of Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Html5Spike::Application.config.secret_token = 'c725b98b14424c5bc5a81e6bfc9168f5204e77d2be066a16fc90d1e9b1ae81fac3f949d4da73fce14109cdcde9f264587c017060ada7157afebde00d49af75c8'
