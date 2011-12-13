171d285eb5903d854529a961b61317fac75ae7d5
Tim McGilchrist <timmcgil@gmail.com>
2011-12-13 16:59:52 +1100

    Silly particle animation to wow the masses.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Html5Spike::Application.routes.draw do
 
   match '/project_list', :controller => :projects, :action => :projects_list
   match '/employee_list', :controller => :employees, :action => :employees_list
+  match '/pretty', :controller => :projects, :action => :pretty
+
   resources :projects
   resources :employees
   resources :worked_ons
+
+
   match '/app.appcache' => Rails::Offline
 end
