6492d0bf70e3dc13ee88656d78452df44fab88eb
Richard Fyffe <richard.fyffe@gmail.com>
2011-12-13 16:26:08 +1100

    fixed problem with switching between pages, and losing data, need to use pageInit and not document.ready in the js

--- app/assets/javascripts/employees/employee-list.js
+++ app/assets/javascripts/employees/employee-list.js
@@ -1,34 +1,40 @@
 
-$(document).ready(function() {
+$("body").live('pageinit',function(event){
 
-    if (navigator.onLine) {
+    console.log("anything???");
 
-        $.getJSON("employee_list.json",  {cache: false},
-           function(json){
-               localStorage.setItem("employees", JSON.stringify(json));
+    if ($("ul.employeelist").length > 0) {
 
-               $.each(json, function(index) {
-                    console.log(json[index]);
-                    $("ul.employeelist").append(createEmployee(json[index]));
-               });
-               $("ul.employeelist").listview('refresh');
-           }
-        );
-    } else {
-        console.log("offline, get data from local storage");
-        //get data from localStorage
-        var json = JSON.parse(localStorage.getItem("employees"));
-        console.log(typeof json);
-        $.each(json, function(index) {
-            console.log(json[index]);
-            $("ul.employeelist").append(createEmployee(json[index]));
-        });
-        $("ul.employeelist").listview('refresh');
+        console.log("run employee javascript");
+
+        if (navigator.onLine) {
+
+            $.getJSON("employee_list.json",  {cache: false},
+               function(json){
+                   localStorage.setItem("employees", JSON.stringify(json));
+
+                   $.each(json, function(index) {
+                        console.log(json[index]);
+                        $("ul.employeelist").append(createEmployee(json[index]));
+                   });
+                   $("ul.employeelist").listview('refresh');
+               }
+            );
+        } else {
+            console.log("offline, get data from local storage");
+            //get data from localStorage
+            var json = JSON.parse(localStorage.getItem("employees"));
+            console.log(typeof json);
+            $.each(json, function(index) {
+                console.log(json[index]);
+                $("ul.employeelist").append(createEmployee(json[index]));
+            });
+            $("ul.employeelist").listview('refresh');
+        }
+
+        function createEmployee(employee) {
+            return $("<li class=\"employee\"><a href=\"#\">" + employee.name + "</a></li>");
+        }
     }
-
-    function createEmployee(employee) {
-        return $("<li class=\"employee\"><a href=\"#\">" + employee.name + "</a></li>");
-    }
-
 
 });
--- app/assets/javascripts/projects/project-list.js
+++ app/assets/javascripts/projects/project-list.js
@@ -1,33 +1,40 @@
 
-$(document).ready(function() {
+$("body").live('pageinit',function(event) {
 
-    if (navigator.onLine) {
+    if ($("ul.projectlist").length > 0) {
 
-        $.getJSON("project_list.json",  {cache: false},
-           function(json){
-               localStorage.setItem("projects", JSON.stringify(json));
-               $.each(json, function(index) {
-                    console.log(json[index]);
-                    $("ul.projectlist").append(createProject(json[index]));
-               })
-               $("ul.projectlist").listview('refresh');
-           }
-        );
+        console.log("run project javascript");
+
+        if (navigator.onLine) {
+
+            $.getJSON("project_list.json",  {cache: false},
+               function(json){
+                   localStorage.setItem("projects", JSON.stringify(json));
+                   $.each(json, function(index) {
+                        console.log(json[index]);
+                        $("ul.projectlist").append(createProject(json[index]));
+                   })
+                   $("ul.projectlist").listview('refresh');
+               }
+            );
+        } else {
+            //get data from localStorage
+            console.log("offline, get data from local storage");
+            //get data from localStorage
+            var json = JSON.parse(localStorage.getItem("employees"));
+            console.log(typeof json);
+            $.each(json, function(index) {
+                console.log(json[index]);
+                $("ul.projectlist").append(createEmployee(json[index]));
+            });
+            $("ul.projectlist").listview('refresh');
+        }
+
+        function createProject(project) {
+            return $("<li class=\"project\"><a href=\"#\">" + project.name + "</a></li>");
+        }
     } else {
-        //get data from localStorage
-        console.log("offline, get data from local storage");
-        //get data from localStorage
-        var json = JSON.parse(localStorage.getItem("employees"));
-        console.log(typeof json);
-        $.each(json, function(index) {
-            console.log(json[index]);
-            $("ul.projectlist").append(createEmployee(json[index]));
-        });
-        $("ul.projectlist").listview('refresh');
-    }
-
-    function createProject(project) {
-        return $("<li class=\"project\"><a href=\"#\">" + project.name + "</a></li>");
+        console.log("fail ");
     }
 
 });
